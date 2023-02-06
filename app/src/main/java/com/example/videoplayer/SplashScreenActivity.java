857e08487f667204f808a2dbec8bb07bff52d50e
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-05 22:01:40 -0500

    update

--- app/src/main/java/com/example/videoplayer/SplashScreenActivity.java
+++ app/src/main/java/com/example/videoplayer/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package com.example.videoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        })
     }
 }
