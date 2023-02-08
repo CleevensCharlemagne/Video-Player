f7692245b5fea5fe6e22a6be6dfbfabdce96c57b
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-08 15:15:15 -0800

    update

--- app/src/main/java/com/example/videoplayer/MediaFiles.java
+++ app/src/main/java/com/example/videoplayer/MediaFiles.java
@@ -2,4 +2,10 @@ package com.example.videoplayer;
 
 public class MediaFiles {
     private String id;
+    private String title;
+    private String id;
+    private String id;
+    private String id;
+    private String id;
+    private String id;
 }
