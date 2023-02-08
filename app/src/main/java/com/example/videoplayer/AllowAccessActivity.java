254ebc90ec3c2e86bd3c5a028ddcc0e5bc84c0cb
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-07 20:48:48 -0500

    permission denied

--- app/src/main/java/com/example/videoplayer/AllowAccessActivity.java
+++ app/src/main/java/com/example/videoplayer/AllowAccessActivity.java
@@ -1,7 +1,16 @@
 package com.example.videoplayer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +18,7 @@ import android.widget.Toast;
 
 public class AllowAccessActivity extends AppCompatActivity {
 
+    public static final int STORAGE_PERMISSION = 1;
     Button allow_btn;
 
     @Override
@@ -20,8 +30,46 @@ public class AllowAccessActivity extends AppCompatActivity {
         allow_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AllowAccessActivity.this, "Allow Access", Toast.LENGTH_SHORT);
+                // Checking if user granted permission or not
+                if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    // If user granted permission
+                    Intent intent = new Intent(AllowAccessActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // If user didn't grant permission
+                    ActivityCompat.requestPermissions(AllowAccessActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+                }
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == STORAGE_PERMISSION){
+            for(int i = 0; i < permissions.length; i++){
+                String permission = permissions[i];
+                if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    boolean showRationale = shouldShowRequestPermissionRationale(permission);
+                    // If user clicked on never ask again
+                    if(!showRationale){
+
+                    }
+                    // User just clicked on denied
+                    else{
+                        ActivityCompat.requestPermissions(AllowAccessActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+                    }
+                } else {
+                    Intent intent = new Intent(AllowAccessActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }
     }
 }
