6d870456b0809c1a8c6d4672eda2e7c8861d6674
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-07 21:44:16 -0500

    alert dialog

--- app/src/main/java/com/example/videoplayer/AllowAccessActivity.java
+++ app/src/main/java/com/example/videoplayer/AllowAccessActivity.java
@@ -2,16 +2,20 @@ package com.example.videoplayer;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,6 +23,7 @@ import android.widget.Toast;
 public class AllowAccessActivity extends AppCompatActivity {
 
     public static final int STORAGE_PERMISSION = 1;
+    public static final int REQUEST_PERMISSION_SETTING = 12;
     Button allow_btn;
 
     @Override
@@ -31,8 +36,8 @@ public class AllowAccessActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Checking if user granted permission or not
-                if(ContextCompat.checkSelfPermission(getApplicationContext(),
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                     // If user granted permission
                     Intent intent = new Intent(AllowAccessActivity.this, MainActivity.class);
                     startActivity(intent);
@@ -50,17 +55,33 @@ public class AllowAccessActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == STORAGE_PERMISSION){
-            for(int i = 0; i < permissions.length; i++){
+        if (requestCode == STORAGE_PERMISSION) {
+            for (int i = 0; i < permissions.length; i++) {
                 String permission = permissions[i];
-                if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
                     boolean showRationale = shouldShowRequestPermissionRationale(permission);
                     // If user clicked on never ask again
-                    if(!showRationale){
+                    if (!showRationale) {
+                        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                        alert.setTitle("Allow Permission")
+                                .setMessage("For playing videos with this App, you must allow access to all videos files on your device."
+                                        + "\n\n" + "Now folow the steps below" + "\n\n"
+                                        + "Open settings form the button bellow" + "\n"
+                                        + "Click on Permissions" + "\n"
+                                        + "Allow access for storage")
+                                .setPositiveButton("Open settings", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                        intent.setData(uri);
+                                        startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
+                                    }
+                                }).create().show();
 
                     }
                     // User just clicked on denied
-                    else{
+                    else {
                         ActivityCompat.requestPermissions(AllowAccessActivity.this,
                                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
                     }
