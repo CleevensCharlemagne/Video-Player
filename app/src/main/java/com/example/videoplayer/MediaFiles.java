7398e4730b397da173f5da7e783cd5b5b6da9e2b
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-08 15:18:23 -0800

    update

--- app/src/main/java/com/example/videoplayer/MediaFiles.java
+++ app/src/main/java/com/example/videoplayer/MediaFiles.java
@@ -8,4 +8,14 @@ public class MediaFiles {
     private String duration;
     private String path;
     private String dateAdded;
+
+    public MediaFiles(String id, String title, String displayName, String size, String duration, String path, String dateAdded) {
+        this.id = id;
+        this.title = title;
+        this.displayName = displayName;
+        this.size = size;
+        this.duration = duration;
+        this.path = path;
+        this.dateAdded = dateAdded;
+    }
 }
