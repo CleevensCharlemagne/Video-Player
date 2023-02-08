5e4550a4119c1af1ca8a4f1dbda9f0c16609ff4d
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-08 15:19:26 -0800

    update

--- app/src/main/java/com/example/videoplayer/MediaFiles.java
+++ app/src/main/java/com/example/videoplayer/MediaFiles.java
@@ -18,4 +18,60 @@ public class MediaFiles {
         this.path = path;
         this.dateAdded = dateAdded;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
 }
