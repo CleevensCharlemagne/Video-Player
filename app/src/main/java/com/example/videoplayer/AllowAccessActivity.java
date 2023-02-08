26641bb21614a050d3c055e0d4be723d8b01a61d
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-07 19:21:17 -0500

    update

--- app/src/main/java/com/example/videoplayer/AllowAccessActivity.java
+++ app/src/main/java/com/example/videoplayer/AllowAccessActivity.java
@@ -3,7 +3,9 @@ package com.example.videoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AllowAccessActivity extends AppCompatActivity {
 
@@ -15,5 +17,11 @@ public class AllowAccessActivity extends AppCompatActivity {
         setContentView(R.layout.activity_allow_access);
 
         allow_btn = findViewById(R.id.allow_access);
+        allow_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText()
+            }
+        });
     }
 }
