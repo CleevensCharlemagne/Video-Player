2b41c31da7610f8dbdf1e62950d35871ecb24f8d
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-08 18:21:14 -0800

    update

--- app/src/main/java/com/example/videoplayer/VideoFoldersAdapter.java
+++ app/src/main/java/com/example/videoplayer/VideoFoldersAdapter.java
@@ -1,4 +1,54 @@
 package com.example.videoplayer;
 
-public class VideoFoldersAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class VideoFoldersAdapter extends RecyclerView.Adapter<VideoFoldersAdapter.ViewHolder> {
+    private ArrayList<MediaFiles> mediaFiles;
+    private ArrayList<String> folderPath;
+    private Context context;
+
+    public VideoFoldersAdapter(ArrayList<MediaFiles> mediaFiles, ArrayList<String> folderPath, Context context) {
+        this.mediaFiles = mediaFiles;
+        this.folderPath = folderPath;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.folder_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView folderName;
+        TextView folderPath;
+        TextView numberOfFiles;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            folderName = itemView.findViewById(R.id.folderName);
+            folderPath = itemView.findViewById(R.id.folderPath);
+            numberOfFiles = itemView.findViewById(R.id.numberOfFiles);
+        }
+    }
 }
