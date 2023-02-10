4128cbf9dceb0cd067db5dd9321c08eb8c2c2cb0
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-10 14:26:54 -0800

    update

--- app/src/main/java/com/example/videoplayer/VideoFilesAdapter.java
+++ app/src/main/java/com/example/videoplayer/VideoFilesAdapter.java
@@ -1,24 +1,9 @@
 package com.example.videoplayer;
 
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class VideoFilesAdapter extends RecyclerView.Adapter<VideoFoldersAdapter.ViewHolder> {
-    @NonNull
-    @Override
-    public VideoFoldersAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
+    public class ViewHolder{
 
-    @Override
-    public void onBindViewHolder(@NonNull VideoFoldersAdapter.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
     }
 }
