483a6cca0078a7e8717500d3309c0a79b1e4c3b0
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-11 17:01:27 -0800

    update

--- app/src/main/java/com/example/videoplayer/VideoFilesAdapter.java
+++ app/src/main/java/com/example/videoplayer/VideoFilesAdapter.java
@@ -40,6 +40,7 @@ public class VideoFilesAdapter extends RecyclerView.Adapter<VideoFilesAdapter.Vi
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         ImageView thumbnail;
         ImageView menu_more;
         TextView videoName;
@@ -48,6 +49,8 @@ public class VideoFilesAdapter extends RecyclerView.Adapter<VideoFilesAdapter.Vi
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            thumbnail = itemView.findViewById(R.id.thumbnail);
         }
     }
 }
